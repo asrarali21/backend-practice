db6691e83a88615205ee2e25dbc10d3cd46b23f3
asrarali21 <mohdasrar3825@gmail.com>
2025-07-19 19:47:14 +0530

    user register logic completed

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,13 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
 import {apiError} from "../utils/apiError.js"
 import { User } from "../models/User.model.js"
+import { uploadoncloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/apiResponse.js"
 
 //custom async handler function to avoid try and catch 
 const registerUser  =  asyncHandler ( async (req , res)=>{ 
     //first we have to take the details from user 
     // validate 
-    // profile on cloudinary
     //check user exist
+    // profile on cloudinary
     //entry in db
 
       const {username , email , password} = req.body
@@ -25,6 +27,28 @@ const registerUser  =  asyncHandler ( async (req , res)=>{
      }
 
      const LocalProfilepath = req.files.path
+
+     const profile = uploadoncloudinary(LocalProfilepath)
+    
+
+     const newUser = await User.create({
+        username,
+        email,
+        password,
+        profilePic : profile.url
+     })
+
+     const createdUser = await User.findById(newUser._id).select(
+        "-password -refreshToken"
+     )
+
+     if (!createdUser) {
+        throw new apiError(401 , "something went wrong registering user")
+     }
+
+     return res.status(200).json(
+        new  ApiResponse(200 , createdUser , "user registred successfully")
+     )
 })
 
 export {registerUser}
