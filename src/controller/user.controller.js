37b86940c5265194038290a54be2aedae164b39a
asrarali21 <mohdasrar3825@gmail.com>
2025-07-26 19:05:59 +0530

    add logout user

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -118,11 +118,30 @@ const loginUser = asyncHandler (async(req , res)=>{
     // exams streak
 
      const logoutuser  = asyncHandler(async(req , res)=>{
-          
+          await User.findByIdAndUpdate(
+            req.user._id,
+            {
+              $set:{
+               refreshToken:undefined
+              }
+            
+          },
+          {
+            new:true
+          }
+         )
+           const options = {
+       httpOnly : true,
+      secure : true 
+      }
+      res.status(200)
+      .clearCookie("accessToken" , options)
+      .clearCookie("refreshToken" , options)
+      .json(new ApiResponse(200 , {}, "user logged out"))
      })
 
 
 })
 
 
-export {registerUser}
+export {registerUser , loginUser , logoutuser}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { upload } from "../middleware/multer.middleware.js";
-import { registerUser } from "../controller/user.controller.js";
+import { loginUser, registerUser } from "../controller/user.controller.js";
+import { verifyJwt } from "../controller/auth.middleware.js";
 
 const router = Router()
 
 router.route("/register").post(upload.single("profilePic"), registerUser)
 
+router.route("/login").post(loginUser)
 
+router.route("/login").post(verifyJwt , logoutuser)
 export {router}
