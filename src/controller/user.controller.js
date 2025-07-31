270756e8c9095c7c59ad29854fb4f2e8319f050d
asrarali21 <mohdasrar3825@gmail.com>
2025-07-31 16:13:54 +0530

    nothing

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -115,8 +115,7 @@ const loginUser = asyncHandler (async(req , res)=>{
          "user logged in successfully"
       )
      )
-    // exams streak
-
+   
      const logoutuser  = asyncHandler(async(req , res)=>{
           await User.findByIdAndUpdate(
             req.user._id,
